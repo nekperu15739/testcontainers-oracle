03d075b15b8d874214604218908b1c7cbde04582
Charly <nekperu15739@users.noreply.github.com>
2019-12-05 10:49:01 +0100

    initial structure

---
+++ src/main/java/org/nekperu15739/testcontainersoracle/TestcontainersOracleApplication.java
@@ -0,0 +1,13 @@
+package org.nekperu15739.testcontainersoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nekperu15739/testcontainersoracle/TestcontainersOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nekperu15739.testcontainersoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
